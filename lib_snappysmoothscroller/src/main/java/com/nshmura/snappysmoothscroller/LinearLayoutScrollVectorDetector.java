e9d4ae29cc45791498ef7b1cf6904a605e22fd15
rahulmandaliya <mandaliya.rahul@gmail.com>
2020-09-10 12:59:28 +0530

    10 Sep 2020 3rd commit

---
+++ lib_snappysmoothscroller/src/main/java/com/nshmura/snappysmoothscroller/LinearLayoutScrollVectorDetector.java
@@ -0,0 +1,19 @@
+package com.nshmura.snappysmoothscroller;
+
+import android.graphics.PointF;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+public class LinearLayoutScrollVectorDetector implements SnappySmoothScroller.ScrollVectorDetector {
+
+    private LinearLayoutManager layoutManager;
+
+    public LinearLayoutScrollVectorDetector(LinearLayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
+
+    @Override
+    public PointF computeScrollVectorForPosition(int targetPosition) {
+        return layoutManager.computeScrollVectorForPosition(targetPosition);
+    }
+}
